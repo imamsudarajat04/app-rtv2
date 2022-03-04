a7852b4e4186b14cf0e89e59ae1dc7789da492f4
imamsudarajat04 <imamsudrajat707@gmail.com>
2022-03-04 20:36:58 +0700

    Request Validation Edit Akun

--- app/Http/Requests/EditAkunRequest.php
+++ app/Http/Requests/EditAkunRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Http\FormRequest;
 
-class SettingProfileRequest extends FormRequest
+class EditAkunRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,30 +24,20 @@ class SettingProfileRequest extends FormRequest
      */
     public function rules()
     {
-        if (in_array($this->method(), ['PUT', 'PATCH'])) {
-            $rules = [
-                'name'    => 'required|string',
-                'no_hp'   => 'required|numeric',
-                'rw'      => 'required|string',
-                'rt'      => 'required|string',
-                'address' => 'required|string',
-                'avatar'  => 'file|image|mimes:jpeg,png,jpg|max:5120',
-            ];
-        } else {
+        if(in_array($this->method(), ['PUT', 'PATCH'])) {
             $rules = [
                 'name'                  => 'required|string',
                 'username'              => 'required|string|unique:users',
-                'password'              => 'required|string|min:5|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{5,}$/',
-                'password_confirmation' => 'required|same:password',
                 'no_hp'                 => 'required|numeric',
                 'rt'                    => 'required|string',
                 'rw'                    => 'required|string',
                 'address'               => 'required|string',
-                'role'                  => 'required|string',
+                'password'              => 'string|min:5|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{5,}$/',
+                'password_confirmation' => 'same:password',
                 'avatar'                => 'file|image|mimes:jpeg,png,jpg|max:5120',
+                'role'                  => 'required|string', 
             ];
         }
-
         return $rules;
     }
 }
