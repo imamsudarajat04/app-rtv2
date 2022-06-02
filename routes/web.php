9572950f988e05b5a03bdc9552b48b5353428680
imamsudarajat04 <imamsudrajat707@gmail.com>
2022-06-02 23:20:37 +0700

    Fungsi Role RT

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('getKecamatan/{id}', 'Admin\DataWargaController@getKecamatan');
 //Kelurahan get id kecamatan
 Route::get('getKelurahan/{id}', 'Admin\DataWargaController@getKelurahan');
 
-Route::group(['middleware' => ['auth','CekRole:superadmin']], function() {
+Route::group(['middleware' => ['auth','CekRole:superadmin,rt']], function() {
     Route::get('/dashboard', "DashboardController@index")->name('dashboard.index');
 
     //Khusus untuk Setting Profile
