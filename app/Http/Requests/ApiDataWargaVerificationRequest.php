a9ee1ad5422485592b5f2b30eef45e88bb86b46a
imamsudarajat04 <imamsudarajat708@gmail.com>
2024-09-13 08:01:12 +0700

    feat: API Search Data Warga Verif

---
+++ app/Http/Requests/ApiDataWargaVerificationRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ApiDataWargaVerificationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "nik" => ["required", "string", "max:16"],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            "nik.required" => "NIK tidak boleh kosong",
+            "nik.string" => "NIK harus berupa string",
+            "nik.max" => "NIK maksimal 16 karakter",
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 'error',
+            'errors' => $validator->errors()
+        ], 422));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,4 @@ Route::post('/store/data-warga', 'Api\ApiStoreDataWargaController@store')->name(
 Route::post('/register/data-warga', 'Api\ApiRegisterWargaController@store')->name('register.api-data-warga');
 Route::post('/store/data-kematian', 'Api\ApiStoreDataKematianController@store')->name('store.api-data-kematian');
 Route::get('/get/data-warga-verif', 'Api\ApiGetDataWargaVerifController@index')->name('get.api-data-warga-verif');
+Route::post('/search/data-warga-verif', 'Api\ApiGetDataWargaVerifController@search')->name('search.api-data-warga-verif');
