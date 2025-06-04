898c46bc5b6d8973ef4efed82df8eda3e0096672
sukma <Ragasukma23@gmail.com>
2025-06-04 23:47:18 +0700

    Data Warga Seeder to insert dummy data (32 Warga & 8 KK)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             GlobalSettingSeeder::class,
             HeaderSettingSeeder::class,
             FooterSettingsSeeder::class,
+            DataWargaSeeder::class,
         ]);
     }
 }
